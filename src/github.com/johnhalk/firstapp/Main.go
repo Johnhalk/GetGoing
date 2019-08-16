fd8c4fbdac5aeefe0151259e55a8a88e3cdde538
John Ashton <johnashton@Eammons-iMac.fios-router.home>
2019-08-16 12:50:18 -0400

    Golang variable declarations

--- src/github.com/johnhalk/firstapp/Main.go
+++ src/github.com/johnhalk/firstapp/Main.go
@@ -2,10 +2,29 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 )
 
 func main() {
-	var i int
-	i = 42
-	fmt.Println(i)
+	var (
+		actorName = "Maisie Williams"
+		noOne = "noone"
+		season = 7
+	)
+	fmt.Println(actorName, noOne, season)
+
+	var (
+		i int = 42
+		j float32
+	)
+
+	j = float32(i)
+
+	fmt.Printf("%v, %T\n", j, j)
+
+	var k string
+	k = strconv.Itoa(i)
+
+	fmt.Printf("%v, %T\n", k, k)
+
 }
